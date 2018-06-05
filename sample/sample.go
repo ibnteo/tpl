e9505a6d3be2418ae999b5894ae28e684676553c
Vladimir Romanovich <ibnteo@gmail.com>
2018-06-06 02:48:00 +0300

    sample

---
+++ sample/sample.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/ibnteo/tpl"
+)
+
+func main() {
+	http.HandleFunc("/", handlerMain)
+	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
+	http.ListenAndServe(":8099", nil)
+}
+
+func handlerMain(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	var info string
+	if r.URL.Query().Get("error") != "" {
+		info = tpl.Format(`<div class="error">%e</div>`, r.URL.Query().Get("error"))
+	}
+
+	tpl.Write(w, `<b style="font-weight:%ept">%e</b>%s`, 10, "<abc>", info)
+
+	tpl.Print("%s\n", info)
+}
